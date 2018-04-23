41b2780f32a0ab512881a764e2b3941ae61101d0
shaobo.hong <shaobo.hong@onebank.com.cn>
2018-04-23 14:12:50 +0800

    测试

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by shaobo on 2018/4/23.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
